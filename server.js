6dd81fa365c46d6bd523a738bf9ed94e997d77fb
NatNichols <natalie.e.nichols@gmail.com>
2024-01-09 00:52:13 -0500

    add root and /home routes

--- server.js
+++ server.js
@@ -13,8 +13,13 @@ const app = express()
 
 
 // mount routes
+app.get('/', function(req, res) {
+  res.send(`<h1>Mulder, it's me</h1>`)
+})
 
-
+app.get('/home', function(req, res) {
+  res.send(`<h1>Home Page</h1>`)
+})
 
 // tell the app to listen on port 3000
 app.listen(3000, function() {
